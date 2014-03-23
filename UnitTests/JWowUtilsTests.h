d383283bc9899cb4cdad5ad71b14f1cc7dd96117
loveyfore@163.com <loveyfore@163.com>
2014-03-23 22:10:19 +0800

    sdfdfd

---
+++ UnitTests/JWowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  JWowUtilsTests.h
+//  Calculator
+//
+//  Created by jin yonghua on 14-3-23.
+//  Copyright (c) 2014年 jin yonghua. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JWowUtilsTests : SenTestCase
+
+@end
---
+++ UnitTests/JWowUtilsTests.m
@@ -0,0 +1,13 @@
+//
+//  JWowUtilsTests.m
+//  Calculator
+//
+//  Created by jin yonghua on 14-3-23.
+//  Copyright (c) 2014年 jin yonghua. All rights reserved.
+//
+
+#import "JWowUtilsTests.h"
+
+@implementation JWowUtilsTests
+
+@end
